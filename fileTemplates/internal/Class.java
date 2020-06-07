7b803feee329dd9a9093a66bd28f63cdf98953ec
zhaoxiang <zhaoxiang@ihsmf.com>
2020-06-07 21:42:10 +0800

    IU-2018.2 <zx_freedom@zx Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, other.xml, Default.xml, find.xml, keymap.xml, zxMethod.xml, memory.view.xml, colors.scheme.xml, databaseDrivers.xml, templates.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, intentionSettings.xml, vcs.xml, toStringTemplates.xml, mybatis.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**  
+    * @author zxfreedom
+    * @description
+    * @date ${DATE}
+    */
+public class ${NAME} {
+}
